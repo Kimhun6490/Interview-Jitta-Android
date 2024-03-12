f8890820e45d41665b0e6bf1f1161953dad4bf2c
DESKTOP-P8O5A4B\Kim <kimhunmalai@gmail.com>
2024-03-12 21:33:00 +0700

    chore: convert compose to xml

--- app/src/main/java/com/mamafarm/android/interview_jitta/MainActivity.kt
+++ app/src/main/java/com/mamafarm/android/interview_jitta/MainActivity.kt
@@ -2,45 +2,15 @@ package com.mamafarm.android.interview_jitta
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.mamafarm.android.interview_jitta.ui.theme.Interview_JittaTheme
+import com.mamafarm.android.interview_jitta.databinding.JittaActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+    private val binding: JittaActivityMainBinding by lazy {
+        JittaActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Interview_JittaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Interview_JittaTheme {
-        Greeting("Android")
+        setContentView(binding.root)
     }
 }
